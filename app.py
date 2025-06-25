d4db8c9c43925d0655abe6d731ae2818c11f4b60
valentinaroj812 <rvrojas@my.loyno.edu>
2025-06-25 19:32:09 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Prueba Mínima")
+
+st.title("✅ App cargada correctamente")
+st.write("Si estás viendo esto, la app funciona en Streamlit Cloud.")
